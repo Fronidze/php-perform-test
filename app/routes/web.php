6e00dcabfbbf475416162959b12a21f7db0ef94c
Fronidze <Fronidze@me.com>
2023-03-09 22:03:50 +0300

    performance changes

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,31 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    $count = 1000;
-//    while ($count > 0) {
-//        /** @var \Faker\Generator $faker */
-//        $faker = app()->make(\Faker\Generator::class);
-//
-//        $user = new \App\Models\User();
-//        $user->name = $faker->name;
-//        $user->email = $faker->email;
-//        $user->password = \Illuminate\Support\Facades\Hash::make($faker->password);
-//
-//        $user->save();
-//        $count--;
-//    }
-
-
-    //$cache = \Illuminate\Support\Facades\Cache::get('test');
-    $count = \Illuminate\Support\Facades\Cache::get('count');
-    if ($count === null) {
-        \App\Models\User::query()
-            ->get()
-            ->each(function (\App\Models\User $user) {});
-
-        $count = \App\Models\User::query()->count();
-        \Illuminate\Support\Facades\Cache::put('count', $count, now()->addSeconds(10));
-    }
-    echo sprintf('count=%d, time=%.2fs', $count, 22.032);
-
+    echo "123";
 });
